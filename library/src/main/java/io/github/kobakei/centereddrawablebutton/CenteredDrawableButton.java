ef03c7fc6eab5e5f7bdd57d6055be82a93807ff5
Keisuke Kobayashi <kkobayashi@coubic.com>
2016-02-29 17:30:14 +0900

    Support color state list

--- library/src/main/java/io/github/kobakei/centereddrawablebutton/CenteredDrawableButton.java
+++ library/src/main/java/io/github/kobakei/centereddrawablebutton/CenteredDrawableButton.java
@@ -1,11 +1,13 @@
 package io.github.kobakei.centereddrawablebutton;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -32,11 +34,15 @@ public class CenteredDrawableButton extends LinearLayout {
 
         CharSequence text = a.getText(R.styleable.CenteredDrawableButton_text);
         int textColor = a.getColor(R.styleable.CenteredDrawableButton_textColor, -1);
+        ColorStateList textColorStateList = a.getColorStateList(R.styleable.CenteredDrawableButton_textColor);
         int textSize = a.getDimensionPixelSize(R.styleable.CenteredDrawableButton_textSize, 0);
 
         ImageView imageView = new ImageView(context, attrs);
         if (drawableLeft != null) {
             imageView.setImageDrawable(drawableLeft);
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            imageView.setVisibility(View.GONE);
         }
         if (drawablePadding > 0) {
             imageView.setPadding(0, 0, drawablePadding, 0);
@@ -47,7 +53,12 @@ public class CenteredDrawableButton extends LinearLayout {
 
         TextView textView = new TextView(context, attrs);
         textView.setText(text);
-        textView.setTextColor(textColor);
+        if (textColor >= 0) {
+            textView.setTextColor(textColor);
+        }
+        if (textColorStateList != null) {
+            textView.setTextColor(textColorStateList);
+        }
         if (textSize > 0) {
             textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
         }
