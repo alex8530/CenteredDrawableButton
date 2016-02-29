ac833bb46123df0339fc6caa552d435255826d94
Keisuke Kobayashi <kkobayashi@coubic.com>
2016-02-29 16:28:24 +0900

    Initial commit

---
+++ app/src/main/java/io/github/kobakei/centereddrawablebutton/sample/MainActivity.java
@@ -0,0 +1,34 @@
+package io.github.kobakei.centereddrawablebutton.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import io.github.kobakei.centereddrawablebutton.CenteredDrawableButton;
+import io.github.kobakei.centereddrawablebuttonsample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CenteredDrawableButton button1 = (CenteredDrawableButton) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked 1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        CenteredDrawableButton button2 = (CenteredDrawableButton) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ library/src/main/java/io/github/kobakei/centereddrawablebutton/CenteredDrawableButton.java
@@ -0,0 +1,62 @@
+package io.github.kobakei.centereddrawablebutton;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Custom button class with centered drawable.
+ * Created by keisukekobayashi on 16/02/29.
+ */
+public class CenteredDrawableButton extends LinearLayout {
+    public CenteredDrawableButton(Context context) {
+        super(context);
+        initializeView(context, null);
+    }
+    public CenteredDrawableButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initializeView(context, attrs);
+    }
+
+    private void initializeView(Context context, AttributeSet attrs) {
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CenteredDrawableButton);
+
+        Drawable drawableLeft = a.getDrawable(R.styleable.CenteredDrawableButton_drawableLeft);
+        int drawablePadding = a.getDimensionPixelSize(R.styleable.CenteredDrawableButton_drawablePadding, 0);
+
+        CharSequence text = a.getText(R.styleable.CenteredDrawableButton_text);
+        int textColor = a.getColor(R.styleable.CenteredDrawableButton_textColor, -1);
+        int textSize = a.getDimensionPixelSize(R.styleable.CenteredDrawableButton_textSize, 0);
+
+        ImageView imageView = new ImageView(context, attrs);
+        if (drawableLeft != null) {
+            imageView.setImageDrawable(drawableLeft);
+        }
+        if (drawablePadding > 0) {
+            imageView.setPadding(0, 0, drawablePadding, 0);
+        }
+        LinearLayout.LayoutParams params1 = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        imageView.setLayoutParams(params1);
+        addView(imageView);
+
+        TextView textView = new TextView(context, attrs);
+        textView.setText(text);
+        textView.setTextColor(textColor);
+        if (textSize > 0) {
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+        }
+        LinearLayout.LayoutParams params2 = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        textView.setLayoutParams(params2);
+        addView(textView);
+
+        setGravity(Gravity.CENTER);
+
+        a.recycle();
+    }
+}
